70cc0ec91023a7409e0662445dd6c413f24b79ac
mdmcginnis <mdlavey@alumni.iastate.edu>
2020-01-26 21:06:40 -0600

    making text responsive

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from './App'
 import 'typeface-roboto';
+import { createMuiTheme, responsiveFontSizes, ThemeProvider } from '@material-ui/core/styles'
 
 
+let theme = createMuiTheme();
+theme = responsiveFontSizes(theme);
 
-render(<App />, document.getElementById('root'))
+render(
+    <div>
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
+    </div>, document.getElementById('root'))
