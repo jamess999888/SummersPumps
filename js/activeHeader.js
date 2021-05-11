f59847ee1e9cbf303a71839a4989c5944179bbf1
mdmcginnis <mdlavey@alumni.iastate.edu>
2021-05-10 21:02:50 -0500

    troubleshooting active link

--- js/activeHeader.js
+++ js/activeHeader.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     var page = root.children[0].id
 
     var headerLink = document.querySelector("a#" + page)
+    console.log(headerLink);
 
     var currentActive = document.querySelector("a.active")
     if (currentActive) {
